4eab26680683f5fd879f3443b59d0bf2cddcd4b9
sydodman <90578929+sydodman@users.noreply.github.com>
2025-05-22 11:49:04 +0100

    V2.0 updated with fast similarity search

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,13 +3,18 @@ import logo from '../assets/fia_logo.png';
 
 export default function Banner({ title }) {
   return (
-    <header className="bg-[rgb(1,44,95)] text-white h-16 flex items-center px-4">
-      <img
-        src={logo}
-        alt="FIA Logo"
-        className="h-11 w-auto mr-4 object-contain"
-      />
-      <h1 className="text-3xl font-FuturaPTMedium">{title}</h1>
+    <header className="bg-[rgb(1,44,95)] text-white h-16 flex items-center justify-between px-4">
+      <div className="flex items-center">
+        <img
+          src={logo}
+          alt="FIA Logo"
+          className="h-11 w-auto mr-4 object-contain"
+        />
+        <h1 className="text-3xl font-FuturaPTMedium">{title}</h1>
+      </div>
+      <div className="text-white text-sm font-FuturaPTLight self-end pb-2 pr-2 opacity-80">
+        v2.0
+      </div>
     </header>
   );
 }
